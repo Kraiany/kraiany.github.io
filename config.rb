44806cf11992029e8c4c5b8d258e643a4e3e42c6
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2017-02-20 17:50:34 +0900

    More useful gems.

--- config.rb
+++ config.rb
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
+require 'slim'
+Slim::Engine.disable_option_validator!
 ###
 # Page options, layouts, aliases and proxies
 ###
 
 set :encoding, 'utf-8'
 set :index_file, 'index.html'
+set :partials_dir, 'partials'
 # Per-page layout changes:
 #
 # With no layout
@@ -86,11 +90,40 @@ end
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
+  activate :sprockets
+  activate :minify_css
+  activate :minify_javascript
+  set :relative_links, true
 
-  # Minify Javascript on build
-  # activate :minify_javascript
+  # somehow minifying html takes some html attributes away so it is causing
+  # some css not applied to certain elements... so until we find alternative
+  # way to monify html, we will disable this
+  activate :minify_html do |html|
+    html.remove_multi_spaces        = true   # Remove multiple spaces
+    html.remove_comments            = true   # Remove comments
+    html.remove_intertag_spaces     = false  # Remove inter-tag spaces
+    html.remove_quotes              = true   # Remove quotes
+    html.simple_doctype             = false  # Use simple doctype
+    html.remove_script_attributes   = false   # Remove script attributes
+    html.remove_style_attributes    = false   # Remove style attributes
+    html.remove_link_attributes     = false   # Remove link attributes
+    html.remove_form_attributes     = false  # Remove form attributes
+    html.remove_input_attributes    = false   # Remove input attributes
+    html.remove_javascript_protocol = false   # Remove JS protocol
+    html.remove_http_protocol       = false   # Remove HTTP protocol
+    html.remove_https_protocol      = false  # Remove HTTPS protocol
+    html.preserve_line_breaks       = false  # Preserve line breaks
+    html.simple_boolean_attributes  = true   # Use simple boolean attributes
+  end
+  activate :asset_hash, :ignore => [/fonts/, /favicon.png/]
+  activate :gzip
+  ignore '*.less'
+  ignore '*_template.*'
+  ignore(/Icon\r$/)
+  ignore(/\.DS_Store/)
+  ignore(/^assets.*\.yml/)
+  ignore(/^assets\/stylesheets\/(?!all).*\.css/)
+  ignore(/^assets\/javascripts\/(?!all).*\.js/)
 end
 
 activate :deploy do |deploy|
