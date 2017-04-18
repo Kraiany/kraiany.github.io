74a31c7aeca0b6f8bbe7839e8a26bf23167b1c09
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2017-04-18 12:00:59 +0900

    Adds assets from current parade project

---
+++ source/assets/javascripts/main.js
@@ -0,0 +1,24 @@
+/*
+ * Change Navbar color while scrolling
+*/
+
+$(window).scroll(function(){
+	handleTopNavAnimation();
+});
+
+$(window).load(function(){
+	handleTopNavAnimation();
+});
+
+function handleTopNavAnimation() {
+	var top=$(window).scrollTop();
+
+	if(top>10){
+		$('#site-nav').addClass('navbar-solid');
+	}
+	else{
+		$('#site-nav').removeClass('navbar-solid');
+	}
+}
+
+smoothScroll.init();
