fa2d25fb1316875f4a127191047a4d18d0064980
Yuri Kovalov <yuri@yurikoval.com>
2025-02-04 13:58:40 +0800

    Add russian-invasion-2025 link for qr

--- config.rb
+++ config.rb
@@ -171,6 +171,7 @@ redirect "landing/nengajou-2023-donors.html", to: "/ja/annual_report_2023.html"
 redirect "landing/nengajou-2023-partners.html", to: "/ja/annual_report_2023.html"
 redirect "landing/nengajou-2024.html", to: "/ja/news/2025-01-01-2025-01-01-newyear.html"
 redirect "landing/tokyo-marathon-2024.html", to: "https://zoom.us/j/98587878390?pwd=Y1BvekNta1JzNkozQ3JnZ2JUc3ltQT09"
+redirect "landing/russian-invasion-2025.html", to: "/ja/news/initiative-category/ukraine-support"
 
 supported_languages.each do |lang|
   redirect "#{lang}/parade/index.html", to: "/#{lang}/parade.html"
