15cd966b7e6bc7874ef57120aff1e4bb1f4797e7
Yuri Kovalov <yuri@yurikoval.com>
2025-07-07 18:08:09 +0800

    Revert "Skip current locale"
    This reverts commit ec16a90fcc1a9935f897493dd7094ccc83e55126.

--- config.rb
+++ config.rb
@@ -50,7 +50,6 @@ helpers do
     dict = {}
 
     data.languages.keys.map(&:to_sym).map do |locale|
-      next if locale == I18n.locale # skip current locale
       localised_path = page.url
         .sub(%r{^/#{I18n.locale}/},"/#{locale}/")
         .sub(%r{/$}, "/index.html")
